e67b91c1d9be4bbccb55aaf8bb2e246708e3afc6
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-07 00:21:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Login with subroutine
+
+def login():
+  while True:
+    username = input("What is your username?: ")
+    password = input("What is your password? ")
+    if username == "Moi" and password == "R":
+      print("Welcome Moi!")
+      break
+    else:
+      print("That is not the correct username or password. Try again!")
+      continue
+    
+print("REPLIT LOGIN SYSTEM")
+login()
